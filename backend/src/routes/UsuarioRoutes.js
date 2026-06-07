55e6c3b2feb0844a6a924558962d5935b8ceb33f
Nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2026-06-07 17:23:36 -0600

    cleaning

--- backend/src/routes/UsuarioRoutes.js
+++ backend/src/routes/UsuarioRoutes.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const QuestionController = require("../controller/QuestionController");
 const AuthController = require("../controller/UsuarioController");
-const questionService = require("../service/QuestionService");
 const authService = require("../service/UsuarioService");
 
 const router = express.Router();
-const questionController = new QuestionController(questionService);
 const authController = new AuthController(authService);
 
-router.get("/Pregunta", questionController.getQuestionById);
-router.get("/Preguntas", questionController.getAllQuestions);
 router.get("/Login", authController.login);
 
 module.exports = router;
