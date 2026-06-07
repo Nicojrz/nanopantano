d55288fc20c33b3795310bb8adf7bc93f018aee9
Nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2026-06-07 16:53:10 -0600

    script 2

--- sql/script.sql
+++ sql/script.sql
@@ -7,23 +7,23 @@ INSERT INTO
 VALUES
     ('admin', '1234', 'administrador');
 
-insert into
+INSERT INTO
     tablajson (columnajson)
-values
+VALUES
     (
         '{"id" : "1","pregunta" : "Una simple pregunta de prueba1","respuesta" : "una simple respuesta de prueba1","drags" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Fidel Castro"},{"imagen" : "https://via.placeholder.com/150","valor" : "George W. Bush"},{"imagen" : "https://via.placeholder.com/150","valor" : "Vidente Fox"},{"imagen" : "https://via.placeholder.com/150","valor" : "Ricardo Lagos"}],"targets" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Mexico"},{"imagen" : "https://via.placeholder.com/150","valor" : "Chile"},{"imagen" : "https://via.placeholder.com/150","valor" : "Cuba"},{"imagen" : "https://via.placeholder.com/150","valor" : "Estados Unidos"}]}'
     );
 
-insert into
+INSERT INTO
     tablajson (columnajson)
-values
+VALUES
     (
         '{"id" : "2","pregunta" : "Una simple pregunta de prueba2","respuesta" : "una simple respuesta de prueba2","drags" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Fidel Castro"},{"imagen" : "https://via.placeholder.com/150","valor" : "George W. Bush"},{"imagen" : "https://via.placeholder.com/150","valor" : "Vidente Fox"},{"imagen" : "https://via.placeholder.com/150","valor" : "Ricardo Lagos"}],"targets" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Mexico"},{"imagen" : "https://via.placeholder.com/150","valor" : "Chile"},{"imagen" : "https://via.placeholder.com/150","valor" : "Cuba"},{"imagen" : "https://via.placeholder.com/150","valor" : "Estados Unidos"}]}'
     );
 
-insert into
+INSERT INTO
     tablajson (columnajson)
-values
+VALUES
     (
         '{"id" : "3","pregunta" : "Una simple pregunta de prueba3","respuesta" : "una simple respuesta de prueba3","drags" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Fidel Castro"},{"imagen" : "https://via.placeholder.com/150","valor" : "George W. Bush"},{"imagen" : "https://via.placeholder.com/150","valor" : "Vidente Fox"},{"imagen" : "https://via.placeholder.com/150","valor" : "Ricardo Lagos"}],"targets" : [{"imagen" : "https://via.placeholder.com/150","valor" : "Mexico"},{"imagen" : "https://via.placeholder.com/150","valor" : "Chile"},{"imagen" : "https://via.placeholder.com/150","valor" : "Cuba"},{"imagen" : "https://via.placeholder.com/150","valor" : "Estados Unidos"}]}'
     );
